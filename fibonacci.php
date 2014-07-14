9d0b119821dc7ff611582a5172f064f3a0fb7d73
agustincl <agustincl@gmail.com>
2014-07-14 14:22:08 +0200

    Serie de Fibonacci. Planteamiento.

---
+++ fibonacci.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Dado un valor, mostar la serie de Fibonacci 
+ * hasta que el elemento actual sea 
+ * menor o igual que el valor dado
+ * Opcional: quitar la coma final.
+ */
+
+$valor = 33;
+echo $f= 0,1,1,2,3,5,8,13,21,
