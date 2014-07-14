b1e3aba243d6cafa77324d153eb1f2c24fc2d724
agustincl <agustincl@gmail.com>
2014-07-14 14:29:30 +0200

    Fibonacci. Solucion.

--- fibonacci.php
+++ fibonacci.php
@@ -6,5 +6,20 @@
  * Opcional: quitar la coma final.
  */
 
-$valor = 33;
-echo $f= 0,1,1,2,3,5,8,13,21,
+$valor = 3334;
+
+echo "f= 0,1,1,2,3,5,8,13,21,";
+
+
+echo "f=0,1,";
+$f0=0;
+$f1=1;
+$f2=$f0+$f1;
+
+while ($f2<=$valor)
+{
+	echo $f2.',';
+	$f0=$f1;
+	$f1=$f2;
+	$f2=$f0+$f1;
+}
