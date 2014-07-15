1ee15bb957fbbcddbe6c593c3f4986202bf0c30b
agustincl <agustincl@gmail.com>
2014-07-15 13:17:33 +0200

    Added convertArray.

--- formularios/action.php
+++ formularios/action.php
@@ -8,6 +8,10 @@ print_r($_FILES);
 echo "</pre>";
 
 
+include ('convertArray.php');
 
-file_put_contents('usuarios.txt', implode('|', $_POST));
 
+file_put_contents('usuarios.txt', convertArray($_POST).'|'.$_FILES['photo']['name']."\n", FILE_APPEND);
+
+
+move_uploaded_file($_FILES['photo']['tmp_name'], $_SERVER['DOCUMENT_ROOT'].'/uploads/'.$_FILES['photo']['name']);
--- formularios/convertArray.php
+++ formularios/convertArray.php
@@ -1,9 +1,17 @@
 <?php
 function convertArray($array)
 {
+	$out=array();
 	foreach($array as $key => $value)
 	{
-		
+		if (is_array($value))
+		{
+			$out[]=implode(',',$value);
+		}
+		else
+		{
+			$out[]=$value;
+		}	
 	}
-	return $array;
+	return implode('|',$out);
 }
